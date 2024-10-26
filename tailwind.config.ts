9aed7065b4addef4a7ab7fcebc7e5d0bda6bebe5
Vera <v.bergstrom83@yahoo.se>
2024-10-26 14:41:42 +0200

    iniate project

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: "#009760",
+        blue: "#1A364C",
+        orange: "#DC840A",
+        ratingText: "#222222",
+        ratingStar: "#F7B559",
+        priceText: "#1A364C",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
