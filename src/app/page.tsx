ae292b37d91e47b8e3496406178a1a4eb2107479
Vera <v.bergstrom83@yahoo.se>
2024-10-28 17:18:42 +0100

    position cards with flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Products from "@/features/products/components/products";
 
 export default function Home() {
-  return (
-    <div>
-      <Products />
-    </div>
-  );
+  return <Products />;
 }
--- src/features/products/components/products.tsx
+++ src/features/products/components/products.tsx
@@ -4,16 +4,20 @@ import ProductCard from "./product-card";
 export default async function Products() {
   const products = await getProducts();
 
-  return products.map((product) => {
-    return (
-      <ProductCard
-        key={product.name}
-        image={product.image}
-        name={product.name}
-        price={product.price}
-        rating={product.rating}
-        color={product.color}
-      />
-    );
-  });
+  return (
+    <div className="flex gap-16">
+      {products.map((product) => {
+        return (
+          <ProductCard
+            key={product.name}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+            rating={product.rating}
+            color={product.color}
+          />
+        );
+      })}
+    </div>
+  );
 }
