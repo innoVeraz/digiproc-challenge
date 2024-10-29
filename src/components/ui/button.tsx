2a865a5f4169c29d51ba3896ef8a05169c4ee126
Vera <v.bergstrom83@yahoo.se>
2024-10-29 11:12:20 +0100

    add button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,22 @@
+import { Product } from "@/features/products/types/products";
+
 type ButtonProps = {
   children: React.ReactNode;
+  bgColor: Product["color"];
 };
 
-export default function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+export default function Button({ children, bgColor }: ButtonProps) {
+  const colorClasses = {
+    orange: "bg-orange",
+    blue: "bg-blue",
+    green: "bg-green",
+  };
+
+  return (
+    <button
+      className={`py-3 rounded-[6px] font-bold text-xs text-white ${colorClasses[bgColor]} w-full`}
+    >
+      {children}
+    </button>
+  );
 }
