dba2ec6e8381acf7e9debfbfc4d5b04dba2ea2c1
Vera <v.bergstrom83@yahoo.se>
2024-10-31 21:26:38 +0100

    add radix modal and cart modal component

--- src/features/shopping-cart/components/cart.tsx
+++ src/features/shopping-cart/components/cart.tsx
@@ -1,22 +1,34 @@
 "use client";
 
 import { useCart } from "@/context/cart-context";
+import { useState } from "react";
 import { IoBagOutline } from "react-icons/io5";
+import CartModal from "./cartModal";
 
 export default function Cart() {
-  const { cartCount } = useCart();
+  const { cartItems } = useCart();
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const cartCount = cartItems.reduce((acc, item) => acc + item.quantity!, 0);
+
   return (
-    <div className="absolute flex items-center top-10 right-10 text-4xl text-blue">
-      <IoBagOutline />
-      {cartCount > 0 && (
-        <div
-          className="absolute top-0 right-0 transform 
+    <div>
+      <div
+        onClick={() => setIsCartOpen(true)}
+        className="absolute flex items-center top-10 right-10 text-4xl text-blue"
+      >
+        <IoBagOutline />
+        {cartCount > 0 && (
+          <div
+            className="absolute top-0 right-0 transform 
         translate-x-1/2 -translate-y-1/2 bg-red-500 text-white 
         rounded-full w-6 h-6 flex items-center justify-center text-sm"
-        >
-          {cartCount}
-        </div>
-      )}
+          >
+            {cartCount}
+          </div>
+        )}
+      </div>
+      <CartModal isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
     </div>
   );
 }
---
+++ src/features/shopping-cart/components/cartModal.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { IoClose } from "react-icons/io5";
+import { useCart } from "@/context/cart-context";
+import Image from "next/image";
+
+type CartModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export default function CartModal({ isOpen, onClose }: CartModalProps) {
+  const { cartItems, cartCount } = useCart();
+
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={onClose}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/70 z-50" />
+
+        <Dialog.Content className="fixed left-1/2 w-full max-w-xs md:top-1/2 md:w-96 lg:w-[50%] lg:max-w-2xl lg:h-[80%] -translate-x-1/2 md:-translate-y-1/2 bg-white rounded-lg p-4 md:p-6 shadow-lg z-50 transition-transform transform scale-100 duration-300 ease-out">
+          <div className="w-full flex justify-between items-center mb-6">
+            <Dialog.Title className="text-lg md:text-2xl font-bold text-center w-full">
+              Varukorgen
+            </Dialog.Title>
+            <Dialog.Close asChild>
+              <button onClick={onClose} className="absolute right-6 top-6">
+                <IoClose className="text-2xl md:text-3xl" />
+              </button>
+            </Dialog.Close>
+          </div>
+
+          {/* Cart Items */}
+          <div className="text-center w-full">
+            {cartCount === 0 ? (
+              <p className="text-base md:text-lg lg:text-xl">
+                Varukorgen är tom
+              </p>
+            ) : (
+              <div className="space-y-4 w-full">
+                {cartItems.map((item) => (
+                  <div
+                    key={item.id}
+                    className="flex items-center justify-center"
+                  >
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      width={200}
+                      height={164}
+                      className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 object-cover rounded mr-4"
+                    />
+                    <div className="flex-1 text-left">
+                      <p className="font-semibold text-lg md:text-xl">
+                        {item.name}
+                      </p>
+                      <p className="text-gray-700">{item.price} SEK</p>
+                      <p className="text-sm text-gray-500">
+                        Antal: {item.quantity}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
